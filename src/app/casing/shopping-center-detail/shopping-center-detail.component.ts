87829a9fc3b30f60d31328b2c2d80f90648a6658
Tyler Tippetts <tyjoti89@gmail.com>
2018-06-20 12:25:38 -0600

    MDS-274 Edit Shopping Center

--- src/app/casing/shopping-center-detail/shopping-center-detail.component.ts
+++ src/app/casing/shopping-center-detail/shopping-center-detail.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ShoppingCenter } from '../../models/full/shopping-center';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ShoppingCenterService } from '../../core/services/shopping-center.service';
+import { MatDialog, MatSnackBar } from '@angular/material';
+import { Location } from '@angular/common';
+import { RoutingStateService } from '../../core/services/routing-state.service';
+import { ErrorService } from '../../core/services/error.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuditingEntity } from '../../models/auditing-entity';
 
 @Component({
   selector: 'mds-shopping-center-detail',
@@ -8,11 +16,74 @@ import { ShoppingCenter } from '../../models/full/shopping-center';
 })
 export class ShoppingCenterDetailComponent implements OnInit {
 
+  loading = false;
   shoppingCenter: ShoppingCenter;
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private shoppingCenterService: ShoppingCenterService,
+              private router: Router,
+              private route: ActivatedRoute,
+              private routingState: RoutingStateService,
+              private errorService: ErrorService,
+              private snackBar: MatSnackBar,
+              private dialog: MatDialog,
+              private _location: Location,
+              private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  private createForm() {
+    this.form = this.fb.group({
+      name: '',
+      owner: ''
+    });
+  }
 
   ngOnInit() {
+    const shoppingCenterId = parseInt(this.route.snapshot.paramMap.get('shoppingCenterId'), 10);
+    this.loadShoppingCenter(shoppingCenterId);
+  }
+
+  private loadShoppingCenter(shoppingCenterId: number) {
+    this.loading = true;
+    this.shoppingCenterService.getOneById(shoppingCenterId)
+      .finally(() => this.loading = false)
+      .subscribe((shoppingCenter: ShoppingCenter) => {
+          this.shoppingCenter = shoppingCenter;
+          this.rebuildForm();
+        },
+        err => this.errorService.handleServerError('Failed to load Shopping Center!', err,
+          () => this.goBack(),
+          () => this.loadShoppingCenter(shoppingCenterId))
+      );
+  }
+
+  private rebuildForm() {
+    this.form.reset(this.shoppingCenter);
+  }
+
+  goBack() {
+    this._location.back();
+  };
+
+  private prepareSaveShoppingCenter(): ShoppingCenter {
+    const saveSC = new ShoppingCenter(this.form.value);
+    const strippedAE = new AuditingEntity(this.shoppingCenter);
+    Object.assign(saveSC, strippedAE);
+    return saveSC;
+  }
+
+  saveForm() {
+    this.shoppingCenterService.update(this.prepareSaveShoppingCenter())
+      .subscribe((shoppingCenter: ShoppingCenter) => {
+        this.shoppingCenter = shoppingCenter;
+        this.rebuildForm();
+        this.snackBar.open(`Successfully updated Shopping Center`, null, {duration: 1000});
+      }, err => this.errorService.handleServerError('Failed to update Shopping Cetner!', err,
+        () => {
+        },
+        () => this.saveForm()));
   }
 
 }
