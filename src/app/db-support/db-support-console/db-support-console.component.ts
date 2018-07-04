3a59b77753afb4cdafcc4120aebe2dbb1b65ba61
Aaron Tippetts <magicmaster85@hotmail.com>
2018-07-04 00:56:38 -0600

    Having Router Issues

--- src/app/db-support/db-support-console/db-support-console.component.ts
+++ src/app/db-support/db-support-console/db-support-console.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Site } from '../../models/full/site';
-import { Duplicate } from '../../models/duplicate';
 
 @Component({
   selector: 'mds-db-support-console',
@@ -8,10 +6,6 @@ import { Duplicate } from '../../models/duplicate';
   styleUrls: ['./db-support-console.component.css']
 })
 export class DbSupportConsoleComponent implements OnInit {
-
-  duplicates: Duplicate<Site>[];
-
-  isLoading = false;
 
   constructor() {
   }
--- src/app/db-support/db-support.module.ts
+++ src/app/db-support/db-support.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+
 import { SharedModule } from '../shared/shared.module';
+
 import { DbSupportRoutingModule } from './db-support-routing.module';
 import { DbSupportDashboardComponent } from './db-support-dashboard/db-support-dashboard.component';
 import { DbSupportConsoleComponent } from './db-support-console/db-support-console.component';
