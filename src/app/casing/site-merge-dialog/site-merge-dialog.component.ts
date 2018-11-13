1c966a1e7c759411d3af6d8a6367e7db4a763d92
Aaron Tippetts <magicmaster85@hotmail.com>
2018-11-13 00:02:14 -0700

    casing-dashboard.component.spec.ts file broke
    Not sure how to fix, so done for tonight.

--- src/app/casing/site-merge-dialog/site-merge-dialog.component.ts
+++ src/app/casing/site-merge-dialog/site-merge-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'mds-site-merge-dialog',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SiteMergeDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
