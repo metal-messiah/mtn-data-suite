ff4694c4ea1f51ee708e90c268abc3fb47235ccb
Tyler Tippetts <tyjoti89@gmail.com>
2019-03-01 12:01:01 -0700

    Ran ng tslint --fix --project=mtn-data-suite. Fixed indentation on all files to 4 spaces. Other fixes applied

--- src/app/shared/file-input/tab-select-dialog/tab-select-dialog.component.ts
+++ src/app/shared/file-input/tab-select-dialog/tab-select-dialog.component.ts
@@ -2,36 +2,36 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef, MatDialog, MatSnackBar } from '@angular/material';
 
 @Component({
-	selector: 'mds-tab-select-dialog',
-	templateUrl: './tab-select-dialog.component.html',
-	styleUrls: [ './tab-select-dialog.component.css' ]
+    selector: 'mds-tab-select-dialog',
+    templateUrl: './tab-select-dialog.component.html',
+    styleUrls: [ './tab-select-dialog.component.css' ]
 })
 export class TabSelectDialogComponent implements OnInit {
-	sheetNames: string[];
-	selectedSheet: string;
+    sheetNames: string[];
+    selectedSheet: string;
 
-	constructor(
-		public dialogRef: MatDialogRef<TabSelectDialogComponent>,
-		public dialog: MatDialog,
-		public snackBar: MatSnackBar,
-		@Inject(MAT_DIALOG_DATA) public data: any
-	) {
-		this.sheetNames = data.sheetNames;
-		this.selectedSheet = data.sheetNames[0];
-		dialogRef.disableClose = true;
-	}
+    constructor(
+        public dialogRef: MatDialogRef<TabSelectDialogComponent>,
+        public dialog: MatDialog,
+        public snackBar: MatSnackBar,
+        @Inject(MAT_DIALOG_DATA) public data: any
+    ) {
+        this.sheetNames = data.sheetNames;
+        this.selectedSheet = data.sheetNames[0];
+        dialogRef.disableClose = true;
+    }
 
-	ngOnInit() {}
+    ngOnInit() {}
 
-	close(): void {
-		this.dialogRef.close();
-	}
+    close(): void {
+        this.dialogRef.close();
+    }
 
-	proceed(): void {
-		this.dialogRef.close(this.selectedSheet);
-	}
+    proceed(): void {
+        this.dialogRef.close(this.selectedSheet);
+    }
 
-	select(sheet) {
-		this.selectedSheet = sheet;
-	}
+    select(sheet) {
+        this.selectedSheet = sheet;
+    }
 }
