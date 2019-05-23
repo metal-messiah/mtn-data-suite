7cdb9e8a4f3acb531694d068edc917afc8af13a0
Jordan Porter <jordan@mtnra.com>
2019-05-23 16:34:48 -0600

    added company info to banner items

--- src/app/casing/select-banner/select-banner.component.ts
+++ src/app/casing/select-banner/select-banner.component.ts
@@ -6,6 +6,9 @@ import { debounceTime, distinctUntilChanged, finalize, map, switchMap } from 'rx
 import { Pageable } from '../../models/pageable';
 import { Banner } from '../../models/full/banner';
 import { BannerService } from '../../core/services/banner.service';
+import { CompanyService } from 'app/core/services/company.service';
+import { SimplifiedCompany } from 'app/models/simplified/simplified-company';
+import { SimplifiedBanner } from 'app/models/simplified/simplified-banner';
 
 @Component({
   selector: 'mds-select-banner',
@@ -14,7 +17,7 @@ import { BannerService } from '../../core/services/banner.service';
 })
 export class SelectBannerComponent implements OnInit {
 
-  banners: Banner[] = [];
+  banners: SimplifiedBanner[] = [];
   totalPages = 0;
   pageNumber = 0;
   bannerQuery: string;
@@ -28,6 +31,7 @@ export class SelectBannerComponent implements OnInit {
   constructor(public dialogRef: MatDialogRef<SelectBannerComponent>,
     private errorService: ErrorService,
     private bannerService: BannerService,
+    private companyService: CompanyService,
     @Inject(MAT_DIALOG_DATA) public data: any) {
     this.remove = data.remove !== undefined ? data.remove : this.remove;
   }
@@ -42,14 +46,14 @@ export class SelectBannerComponent implements OnInit {
       switchMap((value: any) => this.bannerService.getAllByQuery(value))
     );
 
-    typeAhead.subscribe((pageable: Pageable<Banner>) => this.update(pageable, true));
+    typeAhead.subscribe((pageable: Pageable<SimplifiedBanner>) => this.update(pageable, true));
   }
 
   loadMore(): void {
     this.loading = true;
     this.bannerService.getAllByQuery(this.bannerQuery, ++this.pageNumber)
       .pipe(finalize(() => this.loading = false))
-      .subscribe((pageable: Pageable<Banner>) => this.update(pageable, false));
+      .subscribe((pageable: Pageable<SimplifiedBanner>) => this.update(pageable, false));
   }
 
   getBanners(): void {
@@ -57,12 +61,15 @@ export class SelectBannerComponent implements OnInit {
     this.bannerService.getAllByQuery(this.bannerQuery)
       .pipe(finalize(() => this.loading = false))
       .subscribe(
-        (pageable: Pageable<Banner>) => this.update(pageable, true),
+        (pageable: Pageable<SimplifiedBanner>) => {
+
+          this.update(pageable, true)
+        },
         err => console.log(err)
       );
   }
 
-  private update(pageable: Pageable<Banner>, clear: boolean) {
+  private update(pageable: Pageable<SimplifiedBanner>, clear: boolean) {
     this.banners = clear ? pageable.content : this.banners.concat(pageable.content);
     this.totalPages = pageable.totalPages;
     this.pageNumber = pageable.number;
@@ -70,6 +77,14 @@ export class SelectBannerComponent implements OnInit {
 
   getBannerImageSrc(banner) {
     return this.bannerService.getBannerImageSrc(banner);
+  }
+
+  getBannerCompanyName(banner: SimplifiedBanner): string {
+    return banner.companyName;
+  }
+
+  getBannerParentCompanyName(banner: SimplifiedBanner): string {
+    return banner.companyName !== banner.parentCompanyName ? banner.parentCompanyName : ''
   }
 
   selectBanner(banner: Banner) {
