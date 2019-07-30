6a15750df1417ddc7422a73230c11c2cec551e60
Tyler Tippetts <tyjoti89@gmail.com>
2019-07-30 16:15:21 -0600

    Resolved main report pages for saving progress

--- src/app/reports/site-evaluation/site-evaluation.component.ts
+++ src/app/reports/site-evaluation/site-evaluation.component.ts
@@ -11,6 +11,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class SiteEvaluationComponent implements OnInit {
 
   readonly ratingOptions = ['Excellent', 'Good', 'Average', 'Fair', 'Poor', 'TBD'];
+  readonly directions = ['North', 'South', 'East', 'West'];
 
   constructor(private router: Router,
               private snackBar: MatSnackBar,
@@ -18,53 +19,54 @@ export class SiteEvaluationComponent implements OnInit {
               private host: ElementRef) {
   }
 
+  get saving() {
+    return this._rbs.saving;
+  }
+
+  get siteEvaluationNarrativeForm() {
+    return this._rbs.siteEvaluationNarrativeForm;
+  }
+
+  get siteEvaluationRatingsForm() {
+    return this._rbs.siteEvaluationRatingsForm;
+  }
+
   ngOnInit() {
-    if (!this._rbs.getReportTableData()) {
-      setTimeout(() => {
-        this.snackBar.open('No data has been loaded. Starting from the beginning', null, {duration: 2000});
-        this.router.navigate(['reports']);
-      }, 10)
+    if (this._rbs.getReportTableData()) {
+      this.initFormData();
     } else {
-      this.createForm();
-      this.host.nativeElement.scrollTop = 0;
-    }
-  }
-
-  get rbs() {
-    return this._rbs;
-  }
-
-  private createForm() {
-    let cvRecord;
-    if (this._rbs.getReportTableData().currentVolumes) {
-      cvRecord = this._rbs.getReportTableData().currentVolumes.find(record => {
-        return record.mapKey === this._rbs.getReportTableData().selectedMapKey;
+      this._rbs.loadLatest().subscribe(() => {
+        this.initFormData();
+        this.snackBar.open('Loaded latest model', null, {duration: 2000});
+      }, err => {
+        this.snackBar.open(err, null, {duration: 2000});
+        this.router.navigate(['reports']);
       });
     }
-    const sisterStoresAffected = this._rbs.getReportTableData().storeList
-      .filter(store => {
-        return store.category === 'Company Store' && store.mapKey !== this._rbs.getReportTableData().selectedMapKey
-      })
-      .sort((a, b) => {
-        if (a.storeName === b.storeName) {
-          return a.mapKey - b.mapKey;
-        } else {
-          return a.storeName.localeCompare(b.storeName);
-        }
-      })
-      .map(store => `${store.storeName} ${store.mapKey}`)
-      .join('\r\n');
-
-    if (cvRecord) {
-      this._rbs.siteEvaluationNarrativeForm.get('assumedPower').setValue(cvRecord.assumedPower);
-    }
-    if (sisterStoresAffected) {
-      this._rbs.siteEvaluationNarrativeForm.get('affectedSisterStores').setValue(sisterStoresAffected);
-    }
   }
 
-  next() {
-    this.router.navigate(['reports/table-preview']);
+  private initFormData() {
+    // If the affected sister stores hasn't already been set by a loaded model
+    if (!this._rbs.siteEvaluationNarrativeForm.get('affectedSisterStores').value) {
+      // If there are affected sister stores - add them to the form
+      const sisterStoresAffected = this._rbs.getReportTableData().storeList
+        .filter(store => store.category === 'Company Store' && store.mapKey !== this._rbs.getReportTableData().selectedMapKey)
+        .sort((a, b) => {
+          if (a.storeName === b.storeName) {
+            return a.mapKey - b.mapKey;
+          } else {
+            return a.storeName.localeCompare(b.storeName);
+          }
+        })
+        .map(store => `${store.storeName} ${store.mapKey}`)
+        .join('\r\n');
+
+      if (sisterStoresAffected) {
+        this.siteEvaluationNarrativeForm.get('affectedSisterStores').setValue(sisterStoresAffected);
+      }
+    }
+
+    this.host.nativeElement.scrollTop = 0;
   }
 
 }
