054aa999965ecc20a9ac7e94a04557fb4f56fa85
Jordan Porter <jordan@mtnra.com>
2019-10-07 19:13:15 -0700

    MDS-575 Added fuzzy search component to sidenav, and add, remove, and select dialogs, also sorted the results alphabetically

--- src/app/shared/add-remove-stores-list-dialog/add-remove-stores-list-dialog.component.ts
+++ src/app/shared/add-remove-stores-list-dialog/add-remove-stores-list-dialog.component.ts
@@ -22,28 +22,31 @@ export enum AddRemoveType {
   styleUrls: ['./add-remove-stores-list-dialog.component.css']
 })
 export class AddRemoveStoresListDialogComponent implements OnInit {
-
   AddRemoveType = AddRemoveType;
   type: AddRemoveType;
   storeIds: number[];
 
   selectedListIds = [];
   storeLists: SimplifiedStoreList[] = [];
+  storeListsDisplay: SimplifiedStoreList[] = [];
 
   fetching = false;
   saving = false;
 
-  @ViewChild('selectionList', {static: true}) selectionList: any = {
-    selectedOptions: {selected: []}
+  fuzzyFields: string[] = ['storeListName', 'createdBy.name'];
+
+  @ViewChild('selectionList', { static: true }) selectionList: any = {
+    selectedOptions: { selected: [] }
   };
 
-  constructor(private storeListService: StoreListService,
-              private snackBar: MatSnackBar,
-              private authService: AuthService,
-              private errorService: ErrorService,
-              private dialog: MatDialog,
-              private dialogRef: MatDialogRef<AddRemoveStoresListDialogComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: any) {
+  constructor(
+    private storeListService: StoreListService,
+    private snackBar: MatSnackBar,
+    private errorService: ErrorService,
+    private dialog: MatDialog,
+    private dialogRef: MatDialogRef<AddRemoveStoresListDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
     this.type = data.type;
     this.storeIds = data.storeIds;
   }
@@ -62,29 +65,58 @@ export class AddRemoveStoresListDialogComponent implements OnInit {
     } else {
       options['excludingStoreIds'] = this.storeIds;
     }
-    this.storeListService.getStoreLists(options)
-      .pipe(finalize(() => this.fetching = false))
-      .subscribe(page => {
+    this.storeListService
+      .getStoreLists(options)
+      .pipe(finalize(() => (this.fetching = false)))
+      .subscribe(
+        page => {
           this.storeLists = page.content;
+          this.sortAlphabetically(this.storeLists, 'storeListName');
+          this.storeListsDisplay = this.storeLists;
           if (this.storeLists.length === 0) {
             if (this.type === AddRemoveType.ADD) {
-              this.snackBar.open('Store already belongs to every list!', null, {duration: 2000});
+              this.snackBar.open('Store already belongs to every list!', null, { duration: 2000 });
             } else {
-              this.snackBar.open('Store isn\'t in any list!', null, {duration: 2000});
+              this.snackBar.open("Store isn't in any list!", null, { duration: 2000 });
             }
             this.dialogRef.close();
           }
         },
-        err => this.errorService.handleServerError('Failed to get store lists!', err,
-          () => console.log(err), () => this.getLists()));
+        err =>
+          this.errorService.handleServerError(
+            'Failed to get store lists!',
+            err,
+            () => console.log(err),
+            () => this.getLists()
+          )
+      );
+  }
+
+  handleFuzzySearch(output: [SimplifiedStoreList[], string]) {
+    const [results, term] = output;
+    this.storeListsDisplay = term ? results : this.storeLists;
+    this.sortAlphabetically(this.storeListsDisplay, 'storeListName');
+  }
+
+  sortAlphabetically(array: any[], property: string) {
+    array.sort((a, b) => {
+      if (a[property] > b[property]) {
+        return 1;
+      }
+      if (a[property] < b[property]) {
+        return -1;
+      }
+      return 0;
+    });
   }
 
   createNewList() {
-    this.dialog.open(TextInputDialogComponent, {data: {title: 'New List', placeholder: 'New List Name'}})
+    this.dialog
+      .open(TextInputDialogComponent, { data: { title: 'New List', placeholder: 'New List Name' } })
       .afterClosed()
       .subscribe((text: string) => {
         if (text) {
-          const newStoreList = new StoreList({storeListName: text});
+          const newStoreList = new StoreList({ storeListName: text });
           this.saveNewList(newStoreList);
         }
       });
@@ -92,31 +124,40 @@ export class AddRemoveStoresListDialogComponent implements OnInit {
 
   private saveNewList(newStoreList: StoreList) {
     this.saving = false;
-    this.storeListService.create(newStoreList)
-      .pipe(finalize(() => this.saving = false))
-      .subscribe((storeList: StoreList) => {
-        this.snackBar.open('Successfully created new list', null, {duration: 2000});
-        this.storeLists.push(new SimplifiedStoreList(storeList));
-        this.storeLists.sort((a, b) => a.storeListName.localeCompare(b.storeListName));
-      }, err => this.errorService.handleServerError('Failed to create new list!', err,
-        () => console.log(err),
-        () => this.saveNewList(newStoreList)));
+    this.storeListService
+      .create(newStoreList)
+      .pipe(finalize(() => (this.saving = false)))
+      .subscribe(
+        (storeList: StoreList) => {
+          this.snackBar.open('Successfully created new list', null, { duration: 2000 });
+          this.storeLists.push(new SimplifiedStoreList(storeList));
+          this.storeLists.sort((a, b) => a.storeListName.localeCompare(b.storeListName));
+        },
+        err =>
+          this.errorService.handleServerError(
+            'Failed to create new list!',
+            err,
+            () => console.log(err),
+            () => this.saveNewList(newStoreList)
+          )
+      );
   }
 
   submit() {
     if (this.type === AddRemoveType.ADD) {
       const obs = this.selectedListIds.map(listId => this.storeListService.addStoresToStoreList(listId, this.storeIds));
       forkJoin(obs).subscribe(() => {
-        this.snackBar.open('Successfully added stores to selected lists', null, {duration: 2000});
+        this.snackBar.open('Successfully added stores to selected lists', null, { duration: 2000 });
         this.dialogRef.close();
       });
     } else {
-      const obs = this.selectedListIds.map(listId => this.storeListService.removeStoresFromStoreList(listId, this.storeIds));
+      const obs = this.selectedListIds.map(listId =>
+        this.storeListService.removeStoresFromStoreList(listId, this.storeIds)
+      );
       forkJoin(obs).subscribe(() => {
-        this.snackBar.open('Successfully removed stores from selected lists', null, {duration: 2000});
+        this.snackBar.open('Successfully removed stores from selected lists', null, { duration: 2000 });
         this.dialogRef.close();
       });
     }
   }
-
 }
--- src/app/shared/store-sidenav/sidenav-user-lists/sidenav-user-lists.component.ts
+++ src/app/shared/store-sidenav/sidenav-user-lists/sidenav-user-lists.component.ts
@@ -7,7 +7,7 @@ import { StoreService } from '../../../core/services/store.service';
 import { MapService } from '../../../core/services/map.service';
 import { ErrorService } from '../../../core/services/error.service';
 import { MatDialog, MatSnackBar } from '@angular/material';
-import { finalize } from 'rxjs/operators';
+import { finalize, debounceTime } from 'rxjs/operators';
 import { ConfirmDialogComponent } from '../../confirm-dialog/confirm-dialog.component';
 import { TextInputDialogComponent } from '../../text-input-dialog/text-input-dialog.component';
 import { StoreList } from '../../../models/full/store-list';
@@ -15,6 +15,8 @@ import { SimplifiedStore } from '../../../models/simplified/simplified-store';
 import { LatLng } from '../../../models/latLng';
 import { StorageService } from '../../../core/services/storage.service';
 import { Subscription } from 'rxjs';
+import { FormControl, Validators } from '@angular/forms';
+import * as Fuse from 'fuse.js';
 
 @Component({
   selector: 'mds-sidenav-user-lists',
@@ -22,24 +24,27 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./sidenav-user-lists.component.css']
 })
 export class SidenavUserListsComponent implements OnInit, OnDestroy {
-
   storeLists: SimplifiedStoreList[];
+  storeListsDisplay: SimplifiedStoreList[];
 
   fetching = false;
   saving = false;
 
   listChangeListener: Subscription;
 
-  constructor(private router: Router,
-              private storeListService: StoreListService,
-              private dbEntityMarkerService: DbEntityMarkerService,
-              private storeService: StoreService,
-              private mapService: MapService,
-              private errorService: ErrorService,
-              private snackBar: MatSnackBar,
-              private storageService: StorageService,
-              private dialog: MatDialog) {
-  }
+  fuzzyFields: string[] = ['storeListName', 'createdBy.name'];
+
+  constructor(
+    private router: Router,
+    private storeListService: StoreListService,
+    private dbEntityMarkerService: DbEntityMarkerService,
+    private storeService: StoreService,
+    private mapService: MapService,
+    private errorService: ErrorService,
+    private snackBar: MatSnackBar,
+    private storageService: StorageService,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit() {
     this.storageService.set('casing-dashboard-store-list-view', 'my-store-lists').subscribe();
@@ -52,40 +57,53 @@ export class SidenavUserListsComponent implements OnInit, OnDestroy {
   }
 
   goBack() {
-    this.router.navigate(['casing'], {skipLocationChange: true});
+    this.router.navigate(['casing'], { skipLocationChange: true });
   }
 
   private getUserStoreLists() {
     // Get all store lists user has access to see
     this.fetching = true;
-    this.storeListService.getStoreLists({})
-      .pipe(finalize(() => this.fetching = false))
-      .subscribe(page => this.storeLists = page.content.sort(((a, b) => a.storeListName.localeCompare(b.storeListName))));
+    this.storeListService
+      .getStoreLists({})
+      .pipe(finalize(() => (this.fetching = false)))
+      .subscribe(page => {
+        this.storeLists = page.content.sort((a, b) => a.storeListName.localeCompare(b.storeListName));
+        this.storeListsDisplay = this.storeLists;
+      });
+  }
+
+  handleFuzzySearch(output: [SimplifiedStoreList[], string]) {
+    const [results, term] = output;
+    this.storeListsDisplay = term ? results : this.storeLists;
   }
 
   goToStoreList(storeList: SimplifiedStoreList) {
-    this.router.navigate(['casing', 'list-stores', storeList.id], {skipLocationChange: true});
+    this.router.navigate(['casing', 'list-stores', storeList.id], { skipLocationChange: true });
   }
 
   deleteList(storeList: SimplifiedStoreList): void {
     const confirmed = 'Delete List';
-    this.dialog.open(ConfirmDialogComponent, {
-      data: {
-        title: 'Warning!',
-        question: `You are about to delete ${storeList.storeListName}...`,
-        options: [confirmed]
-      }
-    }).afterClosed().subscribe((choice: string) => {
-      if (choice === confirmed) {
-        this.saving = true;
-        this.storeListService.delete(storeList.id)
-          .pipe(finalize(() => this.saving = false))
-          .subscribe(() => {
-            this.snackBar.open('Successfully deleted storeList', null, {duration: 2000});
-            this.getUserStoreLists();
-          });
-      }
-    });
+    this.dialog
+      .open(ConfirmDialogComponent, {
+        data: {
+          title: 'Warning!',
+          question: `You are about to delete ${storeList.storeListName}...`,
+          options: [confirmed]
+        }
+      })
+      .afterClosed()
+      .subscribe((choice: string) => {
+        if (choice === confirmed) {
+          this.saving = true;
+          this.storeListService
+            .delete(storeList.id)
+            .pipe(finalize(() => (this.saving = false)))
+            .subscribe(() => {
+              this.snackBar.open('Successfully deleted storeList', null, { duration: 2000 });
+              this.getUserStoreLists();
+            });
+        }
+      });
   }
 
   storeListIsCurrentFilter(storeList: SimplifiedStoreList) {
@@ -94,46 +112,67 @@ export class SidenavUserListsComponent implements OnInit, OnDestroy {
   }
 
   createNewList() {
-    this.dialog.open(TextInputDialogComponent, {data: {title: 'New List', placeholder: 'New List Name'}})
-      .afterClosed().subscribe((text: string) => {
-      if (text) {
-        const newStoreList = new StoreList({storeListName: text});
-        this.saveNewList(newStoreList);
-      }
-    });
+    this.dialog
+      .open(TextInputDialogComponent, { data: { title: 'New List', placeholder: 'New List Name' } })
+      .afterClosed()
+      .subscribe((text: string) => {
+        if (text) {
+          const newStoreList = new StoreList({ storeListName: text });
+          this.saveNewList(newStoreList);
+        }
+      });
   }
 
   private saveNewList(newStoreList: StoreList) {
     this.saving = false;
-    this.storeListService.create(newStoreList)
-      .pipe(finalize(() => this.saving = false))
-      .subscribe(() => {
-        this.snackBar.open('Successfully created new list', null, {duration: 2000});
-        this.getUserStoreLists();
-      }, err => this.errorService.handleServerError('Failed to create new list!', err,
-        () => console.log(err),
-        () => this.saveNewList(newStoreList)));
+    this.storeListService
+      .create(newStoreList)
+      .pipe(finalize(() => (this.saving = false)))
+      .subscribe(
+        () => {
+          this.snackBar.open('Successfully created new list', null, { duration: 2000 });
+          this.getUserStoreLists();
+        },
+        err =>
+          this.errorService.handleServerError(
+            'Failed to create new list!',
+            err,
+            () => console.log(err),
+            () => this.saveNewList(newStoreList)
+          )
+      );
   }
 
   renameList(storeList: SimplifiedStoreList) {
     // Open dialog to get new name
-    this.dialog.open(TextInputDialogComponent, {data: {title: 'Rename List', placeholder: storeList.storeListName}})
-      .afterClosed().subscribe((text: string) => {
-      if (text) {
-        if (text === storeList.storeListName) {
-          this.snackBar.open('That is the same name. No change.', null, {duration: 2000});
-        } else {
-          this.saving = true;
-          this.storeListService.renameList(storeList.id, text)
-            .pipe(finalize(() => this.saving = false))
-            .subscribe(() => {
-              this.getUserStoreLists();
-              this.snackBar.open('Successfully renamed storeList', null, {duration: 2000});
-            }, err => this.errorService.handleServerError('Failed to update storeList name!', err,
-              () => console.log(err), () => this.renameList(storeList)));
+    this.dialog
+      .open(TextInputDialogComponent, { data: { title: 'Rename List', placeholder: storeList.storeListName } })
+      .afterClosed()
+      .subscribe((text: string) => {
+        if (text) {
+          if (text === storeList.storeListName) {
+            this.snackBar.open('That is the same name. No change.', null, { duration: 2000 });
+          } else {
+            this.saving = true;
+            this.storeListService
+              .renameList(storeList.id, text)
+              .pipe(finalize(() => (this.saving = false)))
+              .subscribe(
+                () => {
+                  this.getUserStoreLists();
+                  this.snackBar.open('Successfully renamed storeList', null, { duration: 2000 });
+                },
+                err =>
+                  this.errorService.handleServerError(
+                    'Failed to update storeList name!',
+                    err,
+                    () => console.log(err),
+                    () => this.renameList(storeList)
+                  )
+              );
+          }
         }
-      }
-    });
+      });
   }
 
   setStoreListAsFilter(storeList: SimplifiedStoreList) {
@@ -143,16 +182,22 @@ export class SidenavUserListsComponent implements OnInit, OnDestroy {
   zoomToList(storeList: SimplifiedStoreList) {
     if (storeList.storeIds.length) {
       this.dbEntityMarkerService.gettingLocations = true;
-      this.storeService.getAllByIds(storeList.storeIds)
-        .pipe(finalize(() => this.dbEntityMarkerService.gettingLocations = false))
-        .subscribe((stores: SimplifiedStore[]) => {
+      this.storeService
+        .getAllByIds(storeList.storeIds)
+        .pipe(finalize(() => (this.dbEntityMarkerService.gettingLocations = false)))
+        .subscribe(
+          (stores: SimplifiedStore[]) => {
             const storeGeoms = stores.map((s: SimplifiedStore) => new LatLng(s.site.latitude, s.site.longitude));
             this.mapService.fitToPoints(storeGeoms, storeList.storeListName);
-          }, err => this.errorService.handleServerError('Failed to Zoom to List!', err,
-          () => console.log(err),
-          () => this.zoomToList(storeList))
+          },
+          err =>
+            this.errorService.handleServerError(
+              'Failed to Zoom to List!',
+              err,
+              () => console.log(err),
+              () => this.zoomToList(storeList)
+            )
         );
     }
   }
-
 }
